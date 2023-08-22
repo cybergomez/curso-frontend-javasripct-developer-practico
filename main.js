b14a4e3c44708fabaac2a1fce72730cafd319163
cybergomez <3838275+cybergomez@users.noreply.github.com>
2023-08-22 06:31:39 -0500

    Actualizacion de html y css

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoICon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click' , toggleDesktopMenu);
-
-menuEmail.addEventListener('mouseover' , toggleDesktopMenu);
+menuHamIcon.addEventListener('click' , toggleMobileMenu);
+menuCarritoICon.addEventListener('click',toogleMenuAside)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleMenuAside() {
+    aside.classList.toggle('inactive');
 }
 
 // menuEmail.addEventListener('click' , () => {
